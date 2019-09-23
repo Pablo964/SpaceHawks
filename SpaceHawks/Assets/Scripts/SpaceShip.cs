f5f6c124c083c4567b2cf363b3bb2562075fcfcf
Pablo <pablosanchez_964@hotmail.com>
2019-09-24 00:53:51 +0200

    movimientoJugadorYenemigo

---
+++ SpaceHawks/Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    [SerializeField]private float velocidad = 30;
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        if ((transform.position.x >= -5) && (transform.position.x <= 5))
+        {
+            transform.Translate(horizontal * velocidad * Time.deltaTime, 0, 0);
+        }
+    }
+}
